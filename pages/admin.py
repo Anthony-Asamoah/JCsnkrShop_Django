51f647c920e5812a278baadbd17b14e5ad64196f
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-07-25 00:38:25 +0000

    added brand table

--- pages/admin.py
+++ pages/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import text
+from .models import text, brand
 
 
 # Register your models here.
-
-
 @admin.register(text)
 class textAdmin(admin.ModelAdmin):
 	list_display = ['about_body', 'services_body', 'brands_body']
+
+
+@admin.register(brand)
+class brandAdmin(admin.ModelAdmin):
+	list_display = ['name']
+	list_display_links = list_display
+	search_fields = list_display
+
